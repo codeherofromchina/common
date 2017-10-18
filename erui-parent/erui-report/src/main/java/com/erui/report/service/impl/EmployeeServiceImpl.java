ee51f3c30756dbb6688aa1c040a5cc1210747f16
wangxiaodan <wangxiaodan@sheyuan.com>
2017-10-18 16:57:44 +0800

    修改读写分离机制，修改为代码级控制读写库

--- erui-parent/erui-report/src/main/java/com/erui/report/service/impl/EmployeeServiceImpl.java
+++ erui-parent/erui-report/src/main/java/com/erui/report/service/impl/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.erui.report.service.impl;
 
+import com.erui.report.model.EmployeeExample;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -9,33 +11,50 @@ import com.erui.report.dao.EmployeeMapper;
 import com.erui.report.model.Employee;
 import com.erui.report.service.EmployeeService;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-	@Autowired
-	private EmployeeMapper employeeMapper;
+	private EmployeeMapper writeMapper;
+	private EmployeeMapper readMapper;
 
-	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	@Resource(name = "sqlSessionMaster")
+	public void setWriteMapper(SqlSessionTemplate sqlSession) {
+		this.writeMapper = sqlSession.getMapper(EmployeeMapper.class);
+	}
+	@Resource(name = "sqlSessionSlave")
+	public void setReadMapper(SqlSessionTemplate sqlSession) {
+		this.readMapper = sqlSession.getMapper(EmployeeMapper.class);
+	}
+
+
+
+
 	public String getName(Long empId) {
-		Employee emp = employeeMapper.selectByPrimaryKey(empId);
+		Employee emp = readMapper.selectByPrimaryKey(empId);
 		return emp.getName();
 	}
 
-	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+
 	public Employee findById(Long empId) {
-		return employeeMapper.selectByPrimaryKey(empId);
+		return readMapper.selectByPrimaryKey(empId);
 	}
 
-	@Transactional(propagation = Propagation.REQUIRED, readOnly = false, rollbackFor = Exception.class)
+
 	public boolean updateName(Long empId, String name) throws Exception {
+
 		Employee emp = findById(empId);
 		if (emp != null) {
 			emp.setName(name);
-			int updateInt = employeeMapper.updateByPrimaryKeySelective(emp);
+			int updateInt = writeMapper.updateByPrimaryKeySelective(emp);
 			if (true) {
-				throw new Exception("回滚");
+				//throw new Exception("回滚");
 			}
 			return updateInt > 0;
 		}
 		return false;
 	}
+
+
 }
