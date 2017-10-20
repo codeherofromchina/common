8f9170c1171a7e55312bd4d8e5a6eb20ca724efd
lirb <git@lirb>
2017-10-20 22:39:01 +0800

    询单总览接口修改

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/report/CustomCentreController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/report/CustomCentreController.java
@@ -2,6 +2,7 @@ package com.erui.boss.web.report;
 
 import com.erui.comm.DateUtil;
 import com.erui.report.model.InquiryCount;
+import com.erui.report.model.InquiryCountVo;
 import com.erui.report.service.InquiryCountService;
 import com.erui.report.service.OrderCountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 客户中心
@@ -35,14 +39,37 @@ public class CustomCentreController {
     public Object inquiryPandect(int day){
 
         Date startTime = DateUtil.recedeTime(day);
-        System.out.println(startTime);
-        int count = inquiryService.inquiryCountByTime(startTime, new Date());
-        String endTime = DateUtil.format("yyyy-MM-dd hh:mm:ss", new Date());
-        String start = DateUtil.format("yyyy-MM-dd hh:mm:ss", startTime);
-        System.out.println(start+"===="+endTime);
-        double amount = inquiryService.inquiryAmountByTime(startTime, new Date());
+//        Date chainDate = DateUtil.recedeTime(day*2);//环比起始时间
+//        //当期询单数
+//        int count = inquiryService.inquiryCountByTime(startTime, new Date());
+//        //当期询单数环比chain
+//
+//        int chainCount = inquiryService.inquiryCountByTime(chainDate, startTime);
+//        int chain=count-chainCount;
+//        //当期询单金额
+//        double amount = inquiryService.inquiryAmountByTime(startTime, new Date());
+//
+//        //当前产品总数量
+//        int proCount = inquiryService.selectProCountByIsOil(startTime, new Date(),null);
+//        int notOilCount = inquiryService.selectProCountByIsOil(startTime, new Date(),"非油气");
+//        //油气类占比
+//        System.out.println(proCount);
+        //同期产品总数量
+//        int chainProCount = inquiryService.selectProCountByIsOil(chainDate, startTime,null);
+//        int chainNotOilCount = inquiryService.selectProCountByIsOil(chainDate, startTime,"非油气");
+        Map<String,Object> params=new HashMap<String,Object>();
+        params.put("startTime",DateUtil.recedeTime(day));
+        params.put("endTime",new Date());
+//        List<InquiryCountVo> list = inquiryService.selectProTop3(startTime, new Date());
+        List<InquiryCountVo> list = inquiryService.selectProTop3(params);
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i).getProCategory()+list.get(i).getProCount());
+        }
 
-        return amount;
+
+        //查询Top3产品数据
+
+        return 4;
     }
 
 
