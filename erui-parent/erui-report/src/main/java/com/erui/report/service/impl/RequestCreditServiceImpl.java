9ad77e96e39beb81f63c329d11ef587531dd7414
shigs <shigs@keruigroup.com>
2017-10-24 09:22:30 +0800

    应收账款导入类型更改

--- erui-parent/erui-report/src/main/java/com/erui/report/service/impl/RequestCreditServiceImpl.java
+++ erui-parent/erui-report/src/main/java/com/erui/report/service/impl/RequestCreditServiceImpl.java
@@ -25,40 +25,63 @@ public class RequestCreditServiceImpl extends BaseService<RequestCreditMapper> i
 	public Map selectTotal() {
 		return this.readMapper.selectTotal();
 	}
-
+	 /**
+	  * @Author:SHIGS
+	  * @Description 应收账款统计
+	  * @Date:9:14 2017/10/24
+	  * @modified By
+	  */
 	@Override
 	public Map selectRequestTotal(Date startDate, Date endDate) {
 		RequestCreditExample requestCreditExample = new RequestCreditExample();
 		requestCreditExample.createCriteria().andCreateAtBetween(startDate,endDate);
 		return this.readMapper.selectRequestTotal(requestCreditExample);
 	}
-
+	 /**
+	  * @Author:SHIGS
+	  * @Description 应收账款趋势图
+	  * @Date:9:13 2017/10/24
+	  * @modified By
+	  */
 	@Override
 	public List<Map> selectRequestTrend(Date startDate, Date endDate) {
 		RequestCreditExample requestCreditExample = new RequestCreditExample();
 		requestCreditExample.createCriteria().andCreateAtBetween(startDate,endDate);
 		return this.readMapper.selectRequestTrend(requestCreditExample);
 	}
-
+	 /**
+	  * @Author:SHIGS
+	  * @Description 应收账款下月
+	  * @Date:9:13 2017/10/24
+	  * @modified By
+	  */
 	@Override
 	public List<Map> selectRequestNext(Date startDate, Date endDate) {
 		RequestCreditExample requestCreditExample = new RequestCreditExample();
 		requestCreditExample.createCriteria().andBackDateBetween(startDate,endDate);
 		return this.readMapper.selectRequestTrend(requestCreditExample);
 	}
-
+	 /**
+	  * @Author:SHIGS
+	  * @Description 查询销售大区
+	  * @Date:9:13 2017/10/24
+	  * @modified By
+	  */
 	@Override
 	public List<Map> selectArea() {
 		return this.readMapper.selectArea();
 	}
 
 	@Override
-	public ImportDataResponse importData(List<String[]> datas) {
+	public ImportDataResponse importData(List<String[]> datas, boolean testOnly) {
 		ImportDataResponse response = new ImportDataResponse();
 		int size = datas.size();
 		RequestCredit rc = null;
 		for (int index = 0; index < size; index++) {
 			String[] strArr = datas.get(index);
+			if (ExcelUploadTypeEnum.verifyData(strArr, ExcelUploadTypeEnum.REQUEST_CREDIT, response, index + 1)) {
+				continue;
+			}
 			rc = new RequestCredit();
 			rc.setCreditSerialNum(strArr[0]);
 			rc.setOrderNum(strArr[1]);
@@ -69,7 +92,15 @@ public class RequestCreditServiceImpl extends BaseService<RequestCreditMapper> i
 			rc.setCustomerCode(strArr[6]);
 			rc.setExportProName(strArr[7]);
 			rc.setTradeTerms(strArr[8]);
-		//	rc.setCreateAt(strArr[9]);
+			try {
+				rc.setCreateAt(
+						DateUtil.parseString2Date(strArr[9], "yyyy/M/d","yyyy/M/d hh:mm:ss",DateUtil.FULL_FORMAT_STR, DateUtil.SHORT_FORMAT_STR));
+			} catch (Exception e) {
+				logger.error(e.getMessage());
+				response.incrFail();
+				response.pushFailItem(ExcelUploadTypeEnum.REQUEST_CREDIT.getTable(), index + 1, "创建日期日期格式错误");
+				continue;
+			}
 			try {
 				rc.setOrderAmount(new BigDecimal(strArr[10]));
 			} catch (Exception ex) {
@@ -91,7 +122,8 @@ public class RequestCreditServiceImpl extends BaseService<RequestCreditMapper> i
 			rc.setBackResponsePerson(strArr[14]);
 
 			try {
-				rc.setBackDate(DateUtil.parseString2Date(strArr[15], DateUtil.FULL_FORMAT_STR,DateUtil.SHORT_FORMAT_STR));
+				rc.setBackDate(
+						DateUtil.parseString2Date(strArr[15], DateUtil.FULL_FORMAT_STR, DateUtil.SHORT_FORMAT_STR));
 			} catch (Exception e) {
 				logger.error(e.getMessage());
 				response.incrFail();
@@ -100,8 +132,10 @@ public class RequestCreditServiceImpl extends BaseService<RequestCreditMapper> i
 			}
 
 			try {
-				writeMapper.insertSelective(rc);
-				response.incrSuccess();
+				if (!testOnly) {
+					writeMapper.insertSelective(rc);
+					response.incrSuccess();
+				}
 			} catch (Exception e) {
 				response.incrFail();
 				response.pushFailItem(ExcelUploadTypeEnum.REQUEST_CREDIT.getTable(), index + 1, e.getMessage());
