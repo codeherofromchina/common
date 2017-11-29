95d27a6e265bbbcf303edbf788c13bd828d7ed9a
shigs <shigs@keruigroup.com>
2017-11-29 16:20:48 +0800

    跨域问题解决

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/filter/crossDomainFilter.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/filter/crossDomainFilter.java
@@ -35,7 +35,7 @@ public class crossDomainFilter implements Filter {
 //            } else {
 //                httpResponse.addHeader("Access-Control-Allow-Origin", origin);
 //            }
-            httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+            httpResponse.addHeader("Access-Control-Allow-Origin", httpRequest.getHeader("Origin"));
             httpResponse.addHeader("Access-Control-Allow-Headers", "Origin, x-requested-with, Content-Type, Accept,X-Cookie,token");
             httpResponse.addHeader("Access-Control-Allow-Credentials", "true");
             httpResponse.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,OPTIONS,DELETE");
