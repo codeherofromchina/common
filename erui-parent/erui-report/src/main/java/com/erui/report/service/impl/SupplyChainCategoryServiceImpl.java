461fc0950e4637a977e1edb7f9b59055673e0d35
lirb <lirb0121@163.com>
2017-12-21 16:36:51 +0800

    修改供应链品类明细错误

--- erui-parent/erui-report/src/main/java/com/erui/report/service/impl/SupplyChainCategoryServiceImpl.java
+++ erui-parent/erui-report/src/main/java/com/erui/report/service/impl/SupplyChainCategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.erui.report.service.impl;
 import com.erui.report.dao.SupplyChainCategoryMapper;
 import com.erui.report.model.SupplyChainCategory;
 import com.erui.report.model.SupplyChainCategoryExample;
+import com.erui.report.model.SupplyChainReadExample;
 import com.erui.report.service.SupplyChainCategoryService;
 import com.erui.report.util.SupplyCateDetailVo;
 import org.springframework.stereotype.Service;
