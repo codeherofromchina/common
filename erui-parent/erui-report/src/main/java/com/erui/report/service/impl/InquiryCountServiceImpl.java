158b0058e79f1fd904e9b2a565d076476c7132d1
lirb <lirb0121@163.com>
2017-12-21 14:43:50 +0800

    修改寻订单趋时土报价

--- erui-parent/erui-report/src/main/java/com/erui/report/service/impl/InquiryCountServiceImpl.java
+++ erui-parent/erui-report/src/main/java/com/erui/report/service/impl/InquiryCountServiceImpl.java
@@ -704,7 +704,7 @@ public class InquiryCountServiceImpl extends BaseService<InquiryCountMapper> imp
         //封装询订单数据
         Map<String, Map<String, Object>> inqTrend = inqTrendList.parallelStream().collect(Collectors.toMap(vo -> vo.get("datetime").toString(), vo -> vo));
         Map<String, Map<String, Object>> ordTrend = ordTrendList.parallelStream().collect(Collectors.toMap(vo -> vo.get("datetime").toString(), vo -> vo));
-        Map<String, Map<String, Object>> quoteTrend = ordTrendList.parallelStream().collect(Collectors.toMap(vo -> vo.get("datetime").toString(), vo -> vo));
+        Map<String, Map<String, Object>> quoteTrend = quoteTrendList.parallelStream().collect(Collectors.toMap(vo -> vo.get("datetime").toString(), vo -> vo));
 
         List<Integer> inqCounts = new ArrayList<>();
         List<Integer> ordCounts = new ArrayList<>();
