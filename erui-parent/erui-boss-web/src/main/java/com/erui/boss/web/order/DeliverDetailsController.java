921080d392d419a7976eb116639f0cd763b84335
Administrator <987709583@qq.com>
2018-01-03 17:17:42 +0800

    修改数据库、实体统一问题

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/DeliverDetailsController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/DeliverDetailsController.java
@@ -2,17 +2,22 @@ package com.erui.boss.web.order;
 
 import com.erui.boss.web.util.Result;
 import com.erui.boss.web.util.ResultStatusEnum;
+import com.erui.order.entity.DeliverConsignGoods;
 import com.erui.order.entity.DeliverDetail;
+import com.erui.order.entity.DeliverNotice;
+import com.erui.order.entity.Goods;
 import com.erui.order.requestVo.DeliverD;
 import com.erui.order.service.DeliverDetailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangxiaodan on 2017/12/11.
@@ -50,12 +55,49 @@ public class DeliverDetailsController {
      *
      * @return
      */
-    //TODO 待掉接口
+    //TODO 掉接口
 
-    @RequestMapping(value = "outboundDetailsPage", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
-    public Result<Object> outboundDetailsPage(){
+    @RequestMapping(value = "outboundDetailsPage", method = RequestMethod.POST)
+    public Result<Object> outboundDetailsPage(@RequestParam(name = "id", required = true) Integer id){
+        DeliverDetail deliverDetail = deliverDetailService.findDetailById(id);
+        Map<String, Object> data = new HashMap<>();
+        // 出库基本信息
+        data.put("deliverDetailInfo", deliverDetail); // 出库基本信息
+        DeliverNotice deliverNotice = deliverDetail.getDeliverNotice();
 
-        return new Result<>(null);
+        Map<String, Object> deliverNoticeInfo = new HashMap<>();
+        deliverNoticeInfo.put("id", deliverNotice.getId()); // 发货通知单ID
+        deliverNoticeInfo.put("tradeTerms", deliverNotice.getTradeTerms()); // 贸易术语
+        deliverNoticeInfo.put("toPlace", deliverNotice.getToPlace()); // 目的港
+        deliverNoticeInfo.put("numers", deliverNotice.getNumers()); // 总包装件数
+
+        List<DeliverConsignGoods> deliverConsignGoodsList = deliverDetail.getDeliverConsignGoodsList();
+        List<Map<String, Object>> goodsInfoList = new ArrayList<>();
+        for (DeliverConsignGoods deliverConsignGoods : deliverConsignGoodsList) {
+            Goods goods = deliverConsignGoods.getGoods();
+
+            Map<String, Object> goodsInfoMap = new HashMap<>();
+            goodsInfoMap.put("goodsId", goods.getId());
+            goodsInfoMap.put("contractNo", goods.getContractNo()); // 销售合同号
+            goodsInfoMap.put("projectNo", goods.getProjectNo()); // 项目号
+            goodsInfoMap.put("sku", goods.getSku()); // 平台SKU
+            goodsInfoMap.put("nameEn", goods.getNameEn()); // 英文品名
+            goodsInfoMap.put("nameZh", goods.getNameZh()); // 中文品名
+            goodsInfoMap.put("model", goods.getModel()); // 规格型号
+            goodsInfoMap.put("unit", goods.getUnit()); // 单位
+            goodsInfoMap.put("sendNum", deliverConsignGoods.getSendNum()); // 数量
+            goodsInfoMap.put("packRequire", deliverConsignGoods.getPackRequire()); // 包装要求
+            goodsInfoMap.put("clientDesc", goods.getClientDesc());  // 描述
+            goodsInfoMap.put("remark", ""); // 备注 TODO
+
+            goodsInfoList.add(goodsInfoMap);
+        }
+
+        deliverNoticeInfo.put("goodsInfo", goodsInfoList);
+        data.put("deliverNoticeInfo", deliverNoticeInfo);// 出库通知单基本信息
+
+
+        return new Result<>(data);
     }
 
 
@@ -64,12 +106,49 @@ public class DeliverDetailsController {
      *
      * @return
      */
-    //TODO 待掉接口
+    //TODO 掉接口
 
-    @RequestMapping(value = "outboundDetails", method = RequestMethod.POST, produces = {"application/json;charset=utf-8"})
-    public Result<Object> outboundDetails(){
+    @RequestMapping(value = "outboundDetails", method = RequestMethod.POST)
+    public Result<Object> outboundDetails(@RequestParam(name = "id", required = true) Integer id){
+        DeliverDetail deliverDetail = deliverDetailService.findDetailById(id);
+        Map<String, Object> data = new HashMap<>();
+        // 出库基本信息
+        data.put("deliverDetailInfo", deliverDetail); // 出库基本信息
+        DeliverNotice deliverNotice = deliverDetail.getDeliverNotice();
 
-        return new Result<>(null);
+        Map<String, Object> deliverNoticeInfo = new HashMap<>();
+        deliverNoticeInfo.put("id", deliverNotice.getId()); // 发货通知单ID
+        deliverNoticeInfo.put("tradeTerms", deliverNotice.getTradeTerms()); // 贸易术语
+        deliverNoticeInfo.put("toPlace", deliverNotice.getToPlace()); // 目的港
+        deliverNoticeInfo.put("numers", deliverNotice.getNumers()); // 总包装件数
+
+        List<DeliverConsignGoods> deliverConsignGoodsList = deliverDetail.getDeliverConsignGoodsList();
+        List<Map<String, Object>> goodsInfoList = new ArrayList<>();
+        for (DeliverConsignGoods deliverConsignGoods : deliverConsignGoodsList) {
+            Goods goods = deliverConsignGoods.getGoods();
+
+            Map<String, Object> goodsInfoMap = new HashMap<>();
+            goodsInfoMap.put("goodsId", goods.getId());
+            goodsInfoMap.put("contractNo", goods.getContractNo()); // 销售合同号
+            goodsInfoMap.put("projectNo", goods.getProjectNo()); // 项目号
+            goodsInfoMap.put("sku", goods.getSku()); // 平台SKU
+            goodsInfoMap.put("nameEn", goods.getNameEn()); // 英文品名
+            goodsInfoMap.put("nameZh", goods.getNameZh()); // 中文品名
+            goodsInfoMap.put("model", goods.getModel()); // 规格型号
+            goodsInfoMap.put("unit", goods.getUnit()); // 单位
+            goodsInfoMap.put("sendNum", deliverConsignGoods.getSendNum()); // 数量
+            goodsInfoMap.put("packRequire", deliverConsignGoods.getPackRequire()); // 包装要求
+            goodsInfoMap.put("clientDesc", goods.getClientDesc());  // 描述
+            goodsInfoMap.put("remark", ""); // 备注 TODO
+
+            goodsInfoList.add(goodsInfoMap);
+        }
+
+        deliverNoticeInfo.put("goodsInfo", goodsInfoList);
+        data.put("deliverNoticeInfo", deliverNoticeInfo);// 出库通知单基本信息
+
+
+        return new Result<>(data);
     }
 
 
