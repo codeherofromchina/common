056969f23dcc9a5559cf376f57a6a33067f4c81a
Administrator <987709583@qq.com>
2018-01-25 16:30:03 +0800

    物流跟踪管理 经办日期查询问题

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/DeliverDetailServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/DeliverDetailServiceImpl.java
@@ -241,7 +241,7 @@ public class DeliverDetailServiceImpl implements DeliverDetailService {
                 }
                 //根据经办日期
                 if (deliverW.getLogisticsDate() != null) {
-                    list.add(cb.equal(root.get("logisticsDate").as(Integer.class), deliverW.getLogisticsDate()));
+                    list.add(cb.equal(root.get("logisticsDate").as(Date.class), deliverW.getLogisticsDate()));
                 }
                 //根据状态
                 if (deliverW.getStatus() != null) {
