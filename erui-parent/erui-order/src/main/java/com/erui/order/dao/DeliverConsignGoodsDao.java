bd15261d4e44ebd1be88e2470c1d978f73f98f3c
Administrator <987709583@qq.com>
2018-01-26 15:35:00 +0800

    出库管理 商品备注保存问题

---
+++ erui-parent/erui-order/src/main/java/com/erui/order/dao/DeliverConsignGoodsDao.java
@@ -0,0 +1,9 @@
+package com.erui.order.dao;
+
+import com.erui.order.entity.DeliverConsignGoods;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.io.Serializable;
+
+public interface DeliverConsignGoodsDao extends JpaRepository<DeliverConsignGoods, Serializable> {
+}
