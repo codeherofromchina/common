9b3d448efd8b54f13b34cab503131a640089be23
wangxiaodan <wangxiaodan@sheyuan.com>
2018-02-07 11:28:55 +0800

    报检单临时附件错误改正

--- erui-parent/erui-order/src/main/java/com/erui/order/dao/InspectApplyTmpAttachDao.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/dao/InspectApplyTmpAttachDao.java
@@ -3,6 +3,7 @@ package com.erui.order.dao;
 import com.erui.order.entity.Area;
 import com.erui.order.entity.InspectApplyTmpAttach;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.io.Serializable;
 import java.util.List;
@@ -11,7 +12,8 @@ import java.util.List;
  * Created by wangxiaodan on 2017/12/11.
  */
 public interface InspectApplyTmpAttachDao extends JpaRepository<InspectApplyTmpAttach, Serializable> {
-    void deleteByInspectApplyId(Integer id);
+    @Query("delete from InspectApplyTmpAttach t where t.inspectApplyId = :inspectApplyId")
+    void deleteByInspectApplyId(Integer inspectApplyId);
 
     // 通过报检ID获取重新报检时临时保存的附件信息
     List<InspectApplyTmpAttach> findByInspectApplyId(Integer inspectApplyId);
