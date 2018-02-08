4c316c980efcb7f6f75a16f8d12814d41589508a
zhangshaopeng <987709583@qq.com>
2018-02-08 15:36:03 +0800

    生成单号问题

--- erui-parent/erui-order/src/main/java/com/erui/order/dao/DeliverDetailDao.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/dao/DeliverDetailDao.java
@@ -3,6 +3,7 @@ package com.erui.order.dao;
 import com.erui.order.entity.DeliverDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.io.Serializable;
 
@@ -12,4 +13,7 @@ import java.io.Serializable;
 public interface DeliverDetailDao extends JpaRepository<DeliverDetail, Serializable>  , JpaSpecificationExecutor<DeliverDetail>{
 
     DeliverDetail findByDeliverDetailNo(String deliverDetailNo);
+
+    @Query(value = "select t.deliver_detail_no from deliver_detail t order by t.deliver_detail_no desc LIMIT 1",nativeQuery = true)
+    String findDeliverDetailNo();
 }
