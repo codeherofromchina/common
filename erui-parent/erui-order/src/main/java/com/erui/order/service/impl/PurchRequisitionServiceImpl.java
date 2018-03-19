4fe56180b02a49153c0490a731030afda54f1fbe
zhangshaopeng <987709583@qq.com>
2018-03-19 15:09:13 +0800

    采购申请模板问题

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchRequisitionServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchRequisitionServiceImpl.java
@@ -207,7 +207,7 @@ public class PurchRequisitionServiceImpl implements PurchRequisitionService {
                     Map<String,String> map= new HashMap();
                     map.put("areaCode","86");
                     map.put("to","[\""+mobile+"\"]");
-                    map.put("content","您好，项目号："+Project1.getProjectNo()+"，商务技术经办人:"+Project1.getBusinessName()+"，已申请采购,请及时处理。感谢您对我们的支持与信任！");
+                    map.put("content","您好，项目号："+Project1.getProjectNo()+"，商务技术经办人："+Project1.getBusinessName()+"，已申请采购,请及时处理。感谢您对我们的支持与信任！");
                     map.put("subType","0");
                     map.put("groupSending","0");
                     map.put("useType","订单");
