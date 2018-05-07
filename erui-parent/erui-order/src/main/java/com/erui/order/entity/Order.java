13adda81d85f1595ae2ed35ec9d51be22e1d3f39
SHIGS <694906743@qq.com>
2018-05-07 17:41:20 +0800

    订单列表英文翻译

--- erui-parent/erui-order/src/main/java/com/erui/order/entity/Order.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/entity/Order.java
@@ -258,6 +258,13 @@ public class Order {
         }
         return null;
     }
+    public String getEnProcessProgressName() {
+        Project.enProjectProgressEnum enProjectProgressEnum = Project.enProjectProgressEnum.enProjectProgressFromCode(getProcessProgress());
+        if (enProjectProgressEnum != null) {
+            return enProjectProgressEnum.getMsg();
+        }
+        return null;
+    }
 
     public String getProcessProgress() {
         return processProgress;
@@ -422,7 +429,17 @@ public class Order {
         }
         return null;
     }
-
+    public String getEnOrderSourceName() {
+        // 1 门户订单 2 门户询单 3 线下订单',
+        if (getOrderSource() == 1) {
+            return "Online orders";
+        } else if (getOrderSource() == 2) {
+            return "BOSS inquiry";
+        } else if (getOrderSource() == 3) {
+            return "Offline orders";
+        }
+        return null;
+    }
     public void setOrderSource(Integer orderSource) {
         this.orderSource = orderSource;
     }
@@ -489,7 +506,13 @@ public class Order {
             return "非油气";
         }
     }
-
+    public String getEnOrderTypeName() {
+        if (getOrderType() == 1) {
+            return "Oil & gas";
+        } else {
+            return "Non-oil or gas";
+        }
+    }
     public void setOrderType(Integer orderType) {
         this.orderType = orderType;
     }
@@ -738,7 +761,17 @@ public class Order {
         }
         return null;
     }
-
+    public String getEnPayStatusName() {
+        // 1:未付款 2:部分付款 3:收款完成
+        if (getPayStatus() == 1) {
+            return "Uncollected";
+        } else if (getPayStatus() == 2) {
+            return "Partly collected";
+        } else if (getPayStatus() == 3) {
+            return "Totally collected";
+        }
+        return null;
+    }
     public void setPayStatus(Integer payStatus) {
         this.payStatus = payStatus;
     }
@@ -754,7 +787,13 @@ public class Order {
         }
         return null;
     }
-
+    public String getEnOrderStatusName() {
+        Order.enStatusEnum enStatusEnum = Order.enFromCode(getStatus());
+        if (enStatusEnum != null) {
+            return enStatusEnum.getMsg();
+        }
+        return null;
+    }
     public void setStatus(Integer status) {
         this.status = status;
     }
@@ -896,6 +935,33 @@ public class Order {
         }
         return null;
     }
+    public static enum enStatusEnum {
+        INIT(1, "To be confirmed"), UNEXECUTED(2, "Not executed"), EXECUTING(3, "Being executed"), DONE(4, "Finished");
 
+        public int code;
+        public String msg;
 
+        enStatusEnum(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+    }
+    public static enStatusEnum enFromCode(Integer code) {
+        if (code != null) {
+            for (enStatusEnum enStatusEnum : enStatusEnum.values()) {
+                if (enStatusEnum.getCode() == code) {
+                    return enStatusEnum;
+                }
+            }
+        }
+        return null;
+    }
 }
