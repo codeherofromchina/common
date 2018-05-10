62b10e77f9c4325cbff1b10b3f67ae8f268144be
wangxiaodan <wangxiaodan@sheyuan.com>
2018-05-10 09:40:57 +0800

    权限验证完善

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/power/AuthorityController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/power/AuthorityController.java
@@ -3,6 +3,7 @@ package com.erui.boss.web.power;
 import com.erui.boss.web.util.Result;
 import com.erui.boss.web.util.ResultStatusEnum;
 import com.erui.power.service.AuthorityService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,22 +19,26 @@ public class AuthorityController {
     private AuthorityService authorityService;
 
     /**
-     *
-     * @param params
-     *      {""userId:"","url":""}
+     * @param params {""userId:"","url":""}
      * @return
      */
     @RequestMapping("validated")
-    public Result<Object> validated(@RequestBody Map<String,String> params) {
+    public Result<Object> validated(@RequestBody Map<String, String> params) {
         String url = params.get("url");
-        Integer userId = Integer.parseInt(params.get("userId"));
+        int userId = 0;
+        String userIdStr = params.get("userId");
+        if (StringUtils.isNumeric(userIdStr)) {
+            userId = Integer.parseInt(userIdStr);
+        }
 
-        if (url != null && userId > 0){
-            if ( authorityService.validate(userId,url) ) {
-                return new Result<>(true);
+        if (StringUtils.isNotBlank(url) && userId > 0) {
+            if (authorityService.validate(userId, url)) {
+                return new Result<>();
+            } else {
+                return new Result<>(ResultStatusEnum.LACK_OF_AUTHORITY);
             }
         }
 
-        return new Result<>(ResultStatusEnum.LACK_OF_AUTHORITY);
+        return new Result<>(ResultStatusEnum.PARAM_ERROR);
     }
 }
