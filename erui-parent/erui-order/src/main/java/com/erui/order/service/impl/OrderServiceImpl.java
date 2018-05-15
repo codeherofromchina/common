4dd05030dd38ee5f6d911318f972cabfa5a3647c
SHIGS <694906743@qq.com>
2018-05-15 10:36:06 +0800

    销售合同号不唯一

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
@@ -518,9 +518,9 @@ public class OrderServiceImpl implements OrderService {
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Integer addOrder(AddOrderVo addOrderVo) throws Exception {
-        if (orderDao.countByContractNo(addOrderVo.getContractNo()) > 0) {
+      /*  if (orderDao.countByContractNo(addOrderVo.getContractNo()) > 0) {
             throw new Exception("销售合同号已存在&&The order No. already exists");
-        }
+        }*/
       /*  if (addOrderVo.getStatus() == Order.StatusEnum.UNEXECUTED.getCode()) {
             // 检查和贸易术语相关字段的完整性
             checkOrderTradeTermsRelationField(addOrderVo);
