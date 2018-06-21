dcf00e2b67c93448d3bb008855e2344ec8acd16f
zhangshaopeng <987709583@qq.com>
2018-06-21 09:56:08 +0800

    财务管理 发货记录，添加首条发货记录

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderAccountServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderAccountServiceImpl.java
@@ -294,11 +294,11 @@ public class OrderAccountServiceImpl implements OrderAccountService {
         // 已收款总金额（USD）   已收款总金额=已收款总金额*汇率          如果收款方式为美元（USD）的话，不用计算汇率
         BigDecimal exchangeRate = order.getExchangeRate();//汇率
         String currencyBn = order.getCurrencyBn();//订单结算币种
-        BigDecimal shipmentsMoney = order.getShipmentsMoney()== null ? BigDecimal.valueOf(0) : order.getShipmentsMoney();  //已发货总金额
+        BigDecimal alreadyGatheringMoney = order.getAlreadyGatheringMoney()== null ? BigDecimal.valueOf(0) : order.getAlreadyGatheringMoney();  //已收款总金额
         if(currencyBn != "USD"){
-            order.setAlreadyGatheringMoneyUSD(shipmentsMoney.multiply(exchangeRate));
+            order.setAlreadyGatheringMoneyUSD(alreadyGatheringMoney.multiply(exchangeRate));
         }else {
-            order.setAlreadyGatheringMoneyUSD(shipmentsMoney);
+            order.setAlreadyGatheringMoneyUSD(alreadyGatheringMoney);
         }
         order.setAlreadyGatheringMoneyUSD(order.getAlreadyGatheringMoneyUSD() == null ? BigDecimal.valueOf(0) : order.getAlreadyGatheringMoneyUSD());
 
