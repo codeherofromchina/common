05b8ebf356a4916a4b9e18c7f8bff7f6b8de4005
zhangshaopeng1015 <987709583@qq.com>
2018-08-21 11:45:53 +0800

    采购çå代办巴办理采购申请 id问题

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
@@ -306,7 +306,8 @@ public class ProjectServiceImpl implements ProjectService {
                     String country = projectUpdate.getCountry();  //国家
                     Map<String, String> bnMapZhCountry = statisticsService.findBnMapZhCountry();
                     newBackLog.setInformTheContent(bnMapZhRegion.get(region)+ " | "+bnMapZhCountry.get(country));  //提示内容
-                    newBackLog.setHostId(projectUpdate.getId());    //父ID，列表页id
+                    newBackLog.setHostId(order.getId());    //父ID，列表页id
+                    newBackLog.setFollowId(projectUpdate.getId());    //子ID，详情中列表页id
                     Integer businessUid = projectUpdate.getBusinessUid();//商务技术经办人id
                     newBackLog.setUid(businessUid);   ////经办人id
                     backLogService.addBackLogByDelYn(newBackLog);
