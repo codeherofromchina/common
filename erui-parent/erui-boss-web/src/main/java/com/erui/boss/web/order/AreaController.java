1cccdbd1947a8351db017c521b68f4e51c150cec
SHIGS <694906743@qq.com>
2018-09-03 15:19:38 +0800

    test MASTER

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/AreaController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/AreaController.java
@@ -6,11 +6,8 @@ import com.erui.order.service.AreaService;
 import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
