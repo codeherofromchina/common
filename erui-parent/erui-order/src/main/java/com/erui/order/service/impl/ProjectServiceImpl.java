f3d1a10bcfae2d93a50b51c790dd04f80338eb59
wangxiaodan <wangxiaodan@sheyuan.com>
2018-09-20 16:57:43 +0800

    修正法务审核时销售合同号判断

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
@@ -1083,8 +1083,8 @@ public class ProjectServiceImpl implements ProjectService {
                         auditingUserId_i = StringUtils.strip(replace, ",");
                     }
                     // 添加销售合同号和海外销售合同号
-                    String contractNo = project.getContractNo();
-                    String contractNoOs = project.getContractNoOs();
+                    String contractNo = paramProject.getContractNo();
+                    String contractNoOs = paramProject.getContractNoOs();
                     if(StringUtils.isBlank(contractNo)) {
                         // 销售合同号不能为空
                         return false;
