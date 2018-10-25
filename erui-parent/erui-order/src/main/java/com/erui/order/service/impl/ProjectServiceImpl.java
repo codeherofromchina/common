b043ae858f7b696b6dc1f9499bcf5060e4fe9470
wangxiaodan <wangxiaodan@sheyuan.com>
2018-10-25 14:52:27 +0800

    去除错误的无用引入包

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
@@ -18,7 +18,6 @@ import com.erui.order.service.CheckLogService;
 import com.erui.order.service.ProjectService;
 import com.erui.order.service.StatisticsService;
 import com.erui.order.util.exception.MyException;
-import com.sun.tools.doclint.Entity;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +33,6 @@ import javax.persistence.criteria.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static com.sun.tools.doclint.Entity.and;
 
 
 /**
