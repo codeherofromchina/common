189a02603b59f408e6555e5172bd8344617afe70
shigs <694905743@qq.com>
2018-11-12 17:37:38 +0800

    钉钉审批通知

--- erui-parent/erui-comm/src/main/java/com/erui/comm/util/http/HttpRequest.java
+++ erui-parent/erui-comm/src/main/java/com/erui/comm/util/http/HttpRequest.java
@@ -1,11 +1,18 @@
 package com.erui.comm.util.http;
 
+import com.alibaba.fastjson.JSONObject;
+import com.erui.comm.ThreadLocalUtil;
+import com.erui.comm.util.CookiesUtil;
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -177,5 +184,52 @@ public class HttpRequest {
         return result;
     }
 
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //获取token
+                String eruiToken = (String) ThreadLocalUtil.getObject();
+                //if (StringUtils.isNotBlank(eruiToken)) {
+                    //try {
+                    // 根据id获取商务经办人信息
+                    String jsonParam = "{\"id\":\"" + "33464" + "\"}";
+                    Map<String, String> header = new HashMap<>();
+                    //header.put(CookiesUtil.TOKEN_NAME, eruiToken);
+                    //auth=adf73QWyZ/BwVqlooWdK0mUHiVS/iEEESkGlt8PrD1C1zDU18EqWBm5QUvA; language=zh; eruirsakey=beafa63ae3f34c5d883b8bfaecded955; eruitoken=8cee92eef2a47fa21ce32e9f29112cbd; JSESSIONID=29386BBBD7B5C0DB66A89123EC20CC30
+                    header.put("Cookie", "auth=adf73QWyZ/BwVqlooWdK0mUHiVS/iEEESkGlt8PrD1C1zDU18EqWBm5QUvA; language=zh; eruirsakey=ed55d2b71d144c0eb6ef45e6793730f9; eruitoken=952c880ba8bef88952307839250094e9; JSESSIONID=4C76CAEF4EDE097918BA1D8E42E2C554");
+                    header.put("Content-Type", "application/json");
+                    header.put("accept", "*/*");
+                    String userInfo = HttpRequest.sendPost("http://api.erui.com/v2/user/info", jsonParam, header);
+                    //钉钉通知接口头信息
+                    Map<String, String> header2 = new HashMap<>();
+                    header2.put("Cookie", "tu=0fe5424cc16e85ff0909c37fc731d65e; language=zh; eruirsakey=b9a04d60e5a04906923486838d9e6bd2; eruitoken=741c65c231e49108ff03e974df482a7c; JSESSIONID=D91073080AA27AE155CC740986AD3B6C");
+                    header2.put("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+                    // 获取商务经办人手机号
+                    JSONObject jsonObject = JSONObject.parseObject(userInfo);
+                    Integer code = jsonObject.getInteger("code");
+                    String userName = null;  //商务经办人手机号
+                    //if (code == 1) {
+                    JSONObject data = jsonObject.getJSONObject("data");
+                    userName = data.getString("name");
+                    //发送短信
+                    StringBuffer stringBuffer = new StringBuffer();
+                    stringBuffer.append("toUser=").append("33464");
 
+                    //【销售合同审批通知】您好！姜洪伟的订单，已申请销售合同审批。
+                    stringBuffer.append("&message=您好！" + userName + "的订单，已申请销售合同审批001。CRM客户代码：" + "shuaiGuo11133"+ "，请您登录BOSS系统及时处理。感谢您对我们的支持与信任！");
+                    stringBuffer.append("&type=toUser");
+                    Long startTime = System.currentTimeMillis();
+                    String s1 = HttpRequest.sendPost("http://dingtalk.erui.com/notice", stringBuffer.toString(), header2);
+                    Long endTime = System.currentTimeMillis();
+                    System.out.println("发送通知耗费时间：" + (endTime - startTime) / 1000);
+                    //}
+                    //} catch (Exception e) {
+                    //    throw new MyException(String.format("%s%s%s", "发送钉钉通知异常失败", Constant.ZH_EN_EXCEPTION_SPLIT_SYMBOL, "Sending SMS exceptions to failure"));
+                    //}
+
+              //  }
+            }
+        }).start();
+    }
 }
--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/ProjectServiceImpl.java
@@ -62,6 +62,8 @@ public class ProjectServiceImpl implements ProjectService {
     private ProjectProfitDao projectProfitDao;
     @Value("#{orderProp[MEMBER_INFORMATION]}")
     private String memberInformation;  //查询人员信息调用接口
+    @Value("#{orderProp[DING_SEND_SMS]}")
+    private String dingSendSms;  //发钉钉通知接口
 
     @Value("#{orderProp[SEND_SMS]}")
     private String sendSms;  //发短信接口
@@ -432,6 +434,8 @@ public class ProjectServiceImpl implements ProjectService {
         projectUpdate.setAuditingLevel(auditingLevel);
         projectUpdate.setAuditingProcess("2,3"); // 2.法务审核、3.财务审核
         projectUpdate.setAuditingUserId("31025,39552"); // 崔荣光、田万全
+        sendDingtalk(project.getOrder(), "31025");
+        sendDingtalk(project.getOrder(), "39552");
         projectUpdate.setAuditingStatus(2); // 审核中
     }
 
@@ -1011,6 +1015,54 @@ public class ProjectServiceImpl implements ProjectService {
         }
     }
 
+    //钉钉通知 审批人
+    public void sendDingtalk(Order order, String user) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //获取token
+                String eruiToken = (String) ThreadLocalUtil.getObject();
+                logger.info("发送短信的用户token:" + eruiToken);
+                if (StringUtils.isNotBlank(eruiToken)) {
+                    //try {
+                    // 根据id获取商务经办人信息
+                    String jsonParam = "{\"id\":\"" + user + "\"}";
+                    Map<String, String> header = new HashMap<>();
+                    header.put("Cookie", eruiToken);
+                    header.put("Content-Type", "application/json");
+                    header.put("accept", "*/*");
+                    String userInfo = HttpRequest.sendPost(memberInformation, jsonParam, header);
+                    logger.info("人员详情返回信息：" + userInfo);
+                    //钉钉通知接口头信息
+                    Map<String, String> header2 = new HashMap<>();
+                    header2.put("Cookie", eruiToken);
+                    header2.put("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+                    // 获取商务经办人手机号
+                    JSONObject jsonObject = JSONObject.parseObject(userInfo);
+                    Integer code = jsonObject.getInteger("code");
+                    String userName = null;  //商务经办人手机号
+                    //if (code == 1) {
+                        JSONObject data = jsonObject.getJSONObject("data");
+                        //userName = data.getString("name");
+                        //发送短信
+                        StringBuffer stringBuffer = new StringBuffer();
+                        stringBuffer.append("toUser=").append(user);
+
+                        //【销售合同审批通知】您好！姜洪伟的订单，已申请销售合同审批。
+                        stringBuffer.append("&message=您好！" + userName + "的订单，已申请销售合同审批001。CRM客户代码：" + order.getCrmCode() + "，请您登录BOSS系统及时处理。感谢您对我们的支持与信任！");
+                        stringBuffer.append("&type=toUser");
+                        Long startTime = System.currentTimeMillis();
+                        String s1 = HttpRequest.sendPost(dingSendSms, stringBuffer.toString(), header2);
+                        Long endTime = System.currentTimeMillis();
+                        System.out.println("发送通知耗费时间：" + (endTime - startTime) / 1000);
+                        logger.info("发送钉钉通知返回状态" + s1);
+                   // }
+                }
+            }
+        }).start();
+
+    }
+
     /**
      * 审核项目操作
      *
@@ -1222,6 +1274,7 @@ public class ProjectServiceImpl implements ProjectService {
         checkLogService.insert(checkLog_i);
         project.setAuditingProcess(auditingProcess_i);
         project.setAuditingUserId(auditingUserId_i);
+        sendDingtalk(project.getOrder(), auditingUserId_i);
         project.setAuditingStatus(auditingStatus_i);
         project.setAudiRemark(auditorIds.toString());
         projectDao.save(project);
