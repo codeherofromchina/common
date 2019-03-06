1ca37ec2b0b0f5007b51ea173635680dbe41f010
shigs <694905743@qq.com>
2019-03-06 19:03:44 +0800

    项目开始时间 时区修改02

--- erui-parent/erui-order/src/main/java/com/erui/order/entity/Project.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/entity/Project.java
@@ -40,7 +40,7 @@ public class Project {
     private String projectName;
 
     @Column(name = "start_date")
-    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date startDate;
     //20180711因事业部订单导入需要修改为文本格式
     //@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
