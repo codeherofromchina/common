a93d734606ec6fffd53960d8b4b0cb34495472d6
shigs <694905743@qq.com>
2019-03-26 20:18:11 +0800

    驳回后提交订单商品重复

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
@@ -1020,7 +1020,7 @@ public class OrderServiceImpl implements OrderService {
 
         }
         CheckLog checkLog_i = null; // 审核日志
-        Order orderUpdate = orderDao.saveAndFlush(order);
+        Order orderUpdate = orderDao.save(order);
         // 处理附件信息 attachmentList 库里存在附件列表 dbAttahmentsMap前端传来参数附件列表
         //order.setAttachmentSet(addOrderVo.getAttachDesc());
         List<Attachment> attachmentList = null;
