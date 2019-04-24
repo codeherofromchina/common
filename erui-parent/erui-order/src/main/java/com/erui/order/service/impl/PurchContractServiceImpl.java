f7ee629682488af20b79d2a999bb73183a159956
huhaiming <987709583@qq.com>
2019-04-24 16:18:59 +0800

    采购合同

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchContractServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchContractServiceImpl.java
@@ -120,7 +120,6 @@ public class PurchContractServiceImpl implements PurchContractService {
             }
             dbPurchContract.setPurchContractSignatoriesList(purchContract.getPurchContractSignatoriesList());
         }
-        List<PurchContractGoods> list = new ArrayList<>();
         if(dbPurchContract.getPurchContractGoodsList() != null && purchContract.getPurchContractGoodsList() != null){//合同商品信息
             for(PurchContractGoods dbpgs : dbPurchContract.getPurchContractGoodsList()){
                 for(PurchContractGoods pgs : purchContract.getPurchContractGoodsList()){
@@ -132,11 +131,9 @@ public class PurchContractServiceImpl implements PurchContractService {
                             pgs.setCreateTime(now);
                         }
                     }
-                    list.add(pgs);
                 }
                 dbpgs.setUpdateTime(now);
             }
-            purchContract.setPurchContractGoodsList(list);
         }
 
         dbPurchContract.setUpdateTime(now);
@@ -278,19 +275,23 @@ public class PurchContractServiceImpl implements PurchContractService {
                 // 判断采购是否超限,预采购数量大于合同数量，则错误
                 if (goods.getPrePurchsedNum() + purchaseNum - oldPurchaseNum > goods.getContractGoodsNum()) {
                     throw new Exception(String.format("%s%s%s", "采购数量超过合同数量【sku :" + goods.getSku() + "】", Constant.ZH_EN_EXCEPTION_SPLIT_SYMBOL, "Quantity of purchase exceeds the number of contracts [SKU: " + goods.getSku() + "]"));
+
                 }
                 if (purchaseNum > 0 && (purchContractGoods.getPurchasePrice() == null || purchContractGoods.getPurchasePrice().compareTo(BigDecimal.ZERO) != 1)) {
                     throw new Exception(String.format("%s%s%s", "要采购的商品单价错误【sku :" + goods.getSku() + "】", Constant.ZH_EN_EXCEPTION_SPLIT_SYMBOL, "Unit price error to be purchased [SKU: " + goods.getSku() + "]"));
+
                 }
 
                 goods.setPrePurchsedNum(goods.getPrePurchsedNum() + purchaseNum - oldPurchaseNum);
                 goodsDao.save(goods);
             } else {
                 throw new Exception(String.format("%s%s%s", "不存在的采购商品信息", Constant.ZH_EN_EXCEPTION_SPLIT_SYMBOL, "Non existent procurement of commodity information"));
+
             }
         }
         if (purchContractGoodsList.size() == 0) {
             throw new Exception(String.format("%s%s%s", "必须存在要采购的商品", Constant.ZH_EN_EXCEPTION_SPLIT_SYMBOL, "There must be goods to be purchased"));
+
         }
         dbPurchContract.setPurchContractGoodsList(purchContractGoodsList);
 
