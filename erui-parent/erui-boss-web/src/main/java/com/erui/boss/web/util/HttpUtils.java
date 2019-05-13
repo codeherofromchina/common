b8979c7e901894b4079e5108b53468139819f772
wangxiaodan <wangxiaodan@sheyuan.com>
2019-05-13 15:55:44 +0800

    导出国家excel时会去除只导出前10行

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/util/HttpUtils.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/util/HttpUtils.java
@@ -2,6 +2,7 @@ package com.erui.boss.web.util;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 /**
  * Created by wangxiaodan on 2018/10/12.
@@ -10,8 +11,8 @@ public class HttpUtils {
     public static void setExcelResponseHeader(HttpServletResponse response, String fileName) throws UnsupportedEncodingException {
 
 
-        fileName = new String(fileName.getBytes(), "ISO8859-1");
-        response.setContentType("application/octet-stream;charset=ISO8859-1");
+//        fileName = new String(fileName.getBytes(), "ISO8859-1");
+        response.setContentType("application/octet-stream;charset=" + Charset.defaultCharset());
         response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
         response.addHeader("Pargam", "no-cache");
         response.addHeader("Cache-Control", "no-cache");
