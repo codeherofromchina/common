052eb5e93c1056837907f7cd9ab63ebe87857291
wangxiaodan <wangxiaodan@sheyuan.com>
2019-05-13 12:01:35 +0800

    空格提交

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/report/v2/OrderStatisticsController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/report/v2/OrderStatisticsController.java
@@ -90,6 +90,7 @@ public class OrderStatisticsController {
             totalMoney = orderStatisticsService.projectTotalMoney(params);
             totalMoney = totalMoney.setScale(2, BigDecimal.ROUND_DOWN);
         } else {
+
             totalMoney = BigDecimal.ZERO;
         }
 //        Map<String, Object> data = new HashMap<>();
