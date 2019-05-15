6a14a437fa0d57b0f8cf3d8e4667485de2d7fabd
huhaiming <987709583@qq.com>
2019-05-15 14:41:20 +0800

    空格提交

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/DeliverConsignServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/DeliverConsignServiceImpl.java
@@ -533,7 +533,7 @@ public class DeliverConsignServiceImpl implements DeliverConsignService {
                     Map<String, String> stringStringMap = getInstockServiceImpl.ssoUser(eruiToken);
                     String submenuId = stringStringMap.get("id");
 
-                    // O42订舱负责人角色下面的人可以看到所有列表信息
+                    // O42订舱负责人角色下面的人可以看到所有列表信息。
                     if(userList != null && userList.contains(Integer.parseInt(submenuId))){
                         Predicate[] predicates = new Predicate[userList.size()*2 + 1];
                         int i = 0;
