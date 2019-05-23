29fd1ddf17a52074288cce9f1bd57f9d7226e42f
shigs <694905743@qq.com>
2019-05-23 20:26:19 +0800

    调用出口通知单id错误

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/IogisticsDataServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/IogisticsDataServiceImpl.java
@@ -236,7 +236,7 @@ public class IogisticsDataServiceImpl implements IogisticsDataService {
                 if (deliverDetail.getDeliverConsign() != null && deliverDetail.getDeliverConsign().getOrder().getOrderSource() == 4) {
                     if (StringUtils.isNotBlank(eruiToken)) {
                         Map<String, Object> jsonMap = new HashMap<>();
-                        jsonMap.put("exportId", deliverDetail.getId());
+                        jsonMap.put("exportId", deliverDetail.getDeliverConsign().getId());
                         jsonMap.put("exportStatus", "SHIPPEND");
                         Map<String, String> header = new HashMap<>();
                         header.put(CookiesUtil.TOKEN_NAME, eruiToken);
