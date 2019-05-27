125a1e024d9e7c1ed51391e92c8d079823fd0544
wangxiaodan <wangxiaodan@sheyuan.com>
2019-05-27 18:52:03 +0800

    将报表中订单状态修改为精确3、4

--- erui-parent/erui-report/src/main/java/com/erui/report/quartz/AreaPerformance.java
+++ erui-parent/erui-report/src/main/java/com/erui/report/quartz/AreaPerformance.java
@@ -38,6 +38,6 @@ public class AreaPerformance {
     }
 
 
-    private static final String INSERT_2019_SQL = "INSERT INTO erui_report.rep_area_performance (`year`, area_bn, area_name, money, count) SELECT 2019, t3.bn AS area_bn, t3.`name` AS area_name , SUM(IFNULL(t1.total_price_usd, 0)) AS totalAmount , COUNT(t1.id) AS totalNum FROM erui_order.`order` t1, erui_order.`project` t2, erui_operation.market_area t3 WHERE (t1.order_category != 1 AND t1.order_category != 3 AND t1.`status` >= 3 AND t1.id = t2.order_id AND t3.lang = 'zh' AND t1.region = t3.bn AND t2.start_date >= '2019-01-01') GROUP BY t3.bn, t3.name";
+    private static final String INSERT_2019_SQL = "INSERT INTO erui_report.rep_area_performance (`year`, area_bn, area_name, money, count) SELECT 2019, t3.bn AS area_bn, t3.`name` AS area_name , SUM(IFNULL(t1.total_price_usd, 0)) AS totalAmount , COUNT(t1.id) AS totalNum FROM erui_order.`order` t1, erui_order.`project` t2, erui_operation.market_area t3 WHERE (t1.order_category != 1 AND t1.order_category != 3 AND t1.`status` in (3,4) AND t1.id = t2.order_id AND t3.lang = 'zh' AND t1.region = t3.bn AND t2.start_date >= '2019-01-01') GROUP BY t3.bn, t3.name";
     private static final String DELETE_2019_SQL = "DELETE FROM erui_report.rep_area_performance WHERE `year` = 2019 ";
 }
