527497725c37ee6ccaa8328bd80e6b25c81de795
huhaiming <987709583@qq.com>
2019-05-30 09:38:47 +0800

    空格提交

--- erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/PurchController.java
+++ erui-parent/erui-boss-web/src/main/java/com/erui/boss/web/order/PurchController.java
@@ -6,7 +6,6 @@ import com.erui.comm.ThreadLocalUtil;
 import com.erui.comm.util.CookiesUtil;
 import com.erui.order.entity.*;
 import com.erui.order.requestVo.PurchParam;
-import com.erui.order.service.CheckLogService;
 import com.erui.order.service.PurchContractService;
 import com.erui.order.service.PurchService;
 import org.slf4j.Logger;
