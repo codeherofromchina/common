99f3f05df7e9a936197f099943959fcb30cb4662
shigs <694905743@qq.com>
2019-06-03 14:15:34 +0800

    新的审核日志为空导出订单审批为空

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/OrderServiceImpl.java
@@ -1076,7 +1076,7 @@ public class OrderServiceImpl implements OrderService {
                 bpmInitVar.put("task_fn_check", task_fn_check);
                 if (StringUtils.isNotBlank(techicalUserNo)) {
                     // ID -> userNo
-                    bpmInitVar.put("assignee_pm",techicalUserNo);
+                    bpmInitVar.put("assignee_pm", techicalUserNo);
                 }
                 JSONObject processResp = null;
                 switch (addOrderVo.getOrderCategory()) {
@@ -1132,7 +1132,7 @@ public class OrderServiceImpl implements OrderService {
                 bpmVar.put("audit_status", "APPROVED");
                 if (StringUtils.isNotBlank(techicalUserNo)) {
                     // ID -> userNo
-                    bpmVar.put("assignee_pm",techicalUserNo);
+                    bpmVar.put("assignee_pm", techicalUserNo);
                 }
                 // 完善订单节点，完成任务
                 BpmUtils.completeTask(addOrderVo.getTaskId(), eruiToken, null, bpmVar, "同意");
@@ -1260,6 +1260,10 @@ public class OrderServiceImpl implements OrderService {
             goods.setOutstockNum(0);
             goods.setDepartment(pGoods.getDepartment());
             goods.setPrice(pGoods.getPrice());
+            //添加属性模板 相关信息
+            goods.setTplNo(pGoods.getTplNo());
+            goods.setTplName(pGoods.getTplName());
+            goods.setAttrs(pGoods.getAttrs());
             goodsList.add(goods);
         }
         //order.setGoodsList(goodsList);
@@ -1379,6 +1383,10 @@ public class OrderServiceImpl implements OrderService {
             goods.setDepartment(pGoods.getDepartment());
             goods.setPrice(pGoods.getPrice());
             goods.setOrder(order);
+            //添加属性模板 相关信息
+            goods.setTplNo(pGoods.getTplNo());
+            goods.setTplName(pGoods.getTplName());
+            goods.setAttrs(pGoods.getAttrs());
             goodsList.add(goods);
         }
         order.setGoodsList(goodsList);
@@ -1410,7 +1418,7 @@ public class OrderServiceImpl implements OrderService {
 
             if (StringUtils.isNotBlank(techicalUserNo)) {
                 // ID -> userNo
-                bpmInitVar.put("assignee_pm",techicalUserNo);
+                bpmInitVar.put("assignee_pm", techicalUserNo);
             }
             bpmInitVar.put("param_contract", order1.getContractNo());
             bpmInitVar.put("task_cm_country", order1.getCountry());
@@ -3187,11 +3195,13 @@ public class OrderServiceImpl implements OrderService {
         if (orderDec.getId() != null) {
             resultCheckLogs = checkLogService.findListByOrderId(orderDec.getId());
             Map<String, CheckLog> map = new LinkedMap<>();
-            for (CheckLog cLog : resultCheckLogs) {
-                if (map.containsKey(cLog.getAuditingProcess() + "_" + cLog.getType())) {
-                    map.remove(cLog.getAuditingProcess() + "_" + cLog.getType());
+            if (resultCheckLogs != null && resultCheckLogs.size() > 0) {
+                for (CheckLog cLog : resultCheckLogs) {
+                    if (map.containsKey(cLog.getAuditingProcess() + "_" + cLog.getType())) {
+                        map.remove(cLog.getAuditingProcess() + "_" + cLog.getType());
+                    }
+                    map.put(cLog.getAuditingProcess() + "_" + cLog.getType(), cLog);
                 }
-                map.put(cLog.getAuditingProcess() + "_" + cLog.getType(), cLog);
             }
             passed = map.values().stream().collect(Collectors.toList());
             if (passed == null) {
