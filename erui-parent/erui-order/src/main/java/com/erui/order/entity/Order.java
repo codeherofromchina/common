bef886b383f2de1b567fc9180a608daebbd96954
shigs <694905743@qq.com>
2019-06-11 09:56:16 +0800

    订单列表导出状态不显示已变更和取消

--- erui-parent/erui-order/src/main/java/com/erui/order/entity/Order.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/entity/Order.java
@@ -345,9 +345,9 @@ public class Order {
     @Column(name = "legal_auditer")
     private String legalAuditer;//法务审核人
     // 流程实例ID
-    @Column(name="process_id")
+    @Column(name = "process_id")
     private String processId;
-    @Column(name="task_id")
+    @Column(name = "task_id")
     private String taskId;
 
 
@@ -760,9 +760,9 @@ public class Order {
                 return "市场询单";
             } else if (getOrderSource() == 3) {
                 return "线下订单";
-            }else if (getOrderSource() == 4) {
-            return "EACP";
-        }
+            } else if (getOrderSource() == 4) {
+                return "EACP";
+            }
         }
         return null;
     }
@@ -1357,7 +1357,7 @@ public class Order {
     }
 
     public static enum StatusEnum {
-        INIT(1, "待确认"), UNEXECUTED(2, "未执行"), EXECUTING(3, "执行中"), DONE(4, "完成");
+        INIT(1, "待确认"), UNEXECUTED(2, "未执行"), EXECUTING(3, "执行中"), DONE(4, "完成"), CHANGE(5, "已变更"), CANCEL(0, "取消");
 
         public int code;
         public String msg;
