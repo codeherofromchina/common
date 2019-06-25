af772bd6110779dbe484fc7eb3cda904f98910dc
shigs <694905743@qq.com>
2019-06-25 17:16:55 +0800

    空格

--- erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchContractServiceImpl.java
+++ erui-parent/erui-order/src/main/java/com/erui/order/service/impl/PurchContractServiceImpl.java
@@ -298,7 +298,6 @@ public class PurchContractServiceImpl implements PurchContractService {
 
         }
         dbPurchContract.setPurchContractGoodsList(purchContractGoodsList);
-
         // 删除不关联的商品信息
         if (dbPurchContractGoodsMap.size() > 0) {
             Collection<PurchContractGoods> values = dbPurchContractGoodsMap.values();
@@ -665,6 +664,7 @@ public class PurchContractServiceImpl implements PurchContractService {
         } else {
             result = new PageImpl<Map<String, Object>>(new ArrayList<>(), pageRequest, 0);
         }
+
         return result;
     }
 
@@ -687,6 +687,7 @@ public class PurchContractServiceImpl implements PurchContractService {
                 Predicate status01 = cb.equal(root.get("status").as(Integer.class), PurchContract.StatusEnum.BEING.getCode());
                 Predicate status02 = cb.equal(root.get("status").as(Integer.class), PurchContract.StatusEnum.EXECUTED.getCode());
                 list.add(cb.or(status01, status02));
+
                 // 根据商品未采购完成的
                 Join<PurchContract, PurchContractGoods> goodsJoin = root.join("purchContractGoodsList");
                 list.add(cb.lt(goodsJoin.get("prePurchContractNum").as(Integer.class), goodsJoin.get("purchaseNum").as(Integer.class)));
